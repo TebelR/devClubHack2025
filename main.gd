a6b1a565ed063393246d89cd7620b6619de19580
Roman Tebel <tebelr@myumanitoba.ca>
2025-02-21 22:12:49 -0600

    Fixed weird doubling up of scenes, login now works

--- main.gd
+++ main.gd
@@ -4,16 +4,30 @@ var current_scene = null
 var cur_lon = 0
 var cur_lat = 0
 var phone_flag = null
-@onready var android_loc_plugin = get_node("AndroidGeolocationPlugin")
+
+@export var android_plugin: AndroidGeolocationPlugin
+
+@onready var eng_main_scene = $Eng_Main_Scene
+@onready var eng_2_scene = $Eng_2nd_Scene
+@onready var eng_base_scene = $Eng_Base_Scene
+@onready var ui = $UI
+@onready var menu_scene = $MenuScene
+@onready var player_scene = $Player
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	hide_all_scenes()
+	
 	if(OS.get_name().contains("Android") or OS.get_name().contains("iOS")):
 		set_portrait_mode()#in the case if a phone running this app
-		android_loc_plugin._request_location_permission()
-		await(android_loc_plugin.android_location_permission_updated)
-		android_loc_plugin.android_location_updated.connect(self._on_location_update)
-		android_loc_plugin._start_geolocation_listener(5000, 0.0)#milisec timeout, minDist
+		android_plugin._request_location_permission()
+		await(android_plugin.android_location_permission_updated)
+		android_plugin.android_location_updated.connect(self._on_location_update)
+		android_plugin._start_geolocation_listener(5000, 0.0)#milisec timeout, minDist
+		
+		#android_plugin.android_location_permission_updated.connect(self._on_location_permission)
+		#android_plugin.android_location_updated.connect(self._on_location_update)
 		phone_flag = true
 	else:
 		set_landscape_mode()#In the case of a PC running this app
@@ -25,11 +39,21 @@ func _ready() -> void:
 	proceedToScene("menu_scene")
 
 
+
 func _on_location_update(location_dictionary: Dictionary) -> void:
 	var cur_lat: float = location_dictionary["latitude"]
 	var cur_lon: float = location_dictionary["longitude"]
 
 
+
+#prevents doubling up of scenes
+func hide_all_scenes():
+	eng_main_scene.hide()
+	eng_2_scene.hide()
+	eng_base_scene.hide()
+	ui.hide()
+	player_scene.hide()
+	menu_scene.hide()
 
 
 func set_portrait_mode():
